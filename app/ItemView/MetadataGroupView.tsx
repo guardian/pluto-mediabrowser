31b2a7db445665c5206a13cdcd0ccc4853feb389
Andy Gallagher <andy.gallagher@theguardian.com>
2020-08-14 15:52:22 +0100

    implementing dropdowns and checkbox groups. Checkbox group needs more work on the labelling

--- app/ItemView/MetadataGroupView.tsx
+++ app/ItemView/MetadataGroupView.tsx
@@ -7,7 +7,8 @@ import {
   Paper,
   FormControl,
   Typography,
-  Grid,
+  Grid, Select, MenuItem,
+  FormGroup, Checkbox, FormControlLabel
 } from "@material-ui/core";
 import {
   VidispineField,
@@ -17,6 +18,7 @@ import { VidispineItem } from "../vidispine/item/VidispineItem";
 import { PlutoCustomData } from "../vidispine/field-group/CustomData";
 import { makeStyles } from "@material-ui/core/styles";
 import ChipInput from "material-ui-chip-input";
+import {CheckBox} from "@material-ui/icons";
 
 interface MetadataGroupViewProps {
   group: VidispineFieldGroup;
@@ -86,10 +88,107 @@ const MetadataGroupView: React.FC<MetadataGroupViewProps> = (props) => {
         );
       // case "lookup":
       //     break;
-      // case "checkbox":
-      //     break;
-      // case "dropdown":
-      //     break;
+      case "checkbox":
+        if(viewHints.values) {
+          return (
+              <FormControl classes={{root: classes.inputField}}>
+                <InputLabel htmlFor={controlId}>{viewHints.name}</InputLabel>
+                <FormGroup id={controlId}>
+                  {
+                    viewHints.values.map((dataPair, idx)=>
+                      <FormControlLabel
+                          label={dataPair.value}
+                          control={
+                            <Checkbox
+                                checked={maybeValues ? maybeValues.includes(dataPair.key) : false}
+                                color="primary"
+                                readOnly={viewHints.readonly || props.readonly}
+                                onChange={(event)=>{
+                                  const newValue = event.target.checked ?
+                                      maybeValues ? maybeValues.concat(dataPair.key) : [dataPair.key] :
+                                      maybeValues ? maybeValues.filter(v=>v!==dataPair.key) : [];
+                                  props.valueDidChange(fieldname, newValue);
+                                } }
+                            />
+                          }/>
+                    )
+                  }
+                </FormGroup>
+              </FormControl>
+          )
+        } else {
+          return (
+              <Grid
+                  container
+                  direction="row"
+                  justify="flex-start"
+                  alignItems="flex-start"
+                  spacing={1}
+              >
+                <Grid item sm={6}>
+                  <Typography>{viewHints.name}</Typography>
+                </Grid>
+
+                <Grid item sm={6}>
+                  <Typography
+                      variant="caption"
+                      id={controlId}
+                      classes={{
+                        root: classes.root,
+                      }}
+                  >
+                    Warning: field is a checkbox group but has no values configured
+                  </Typography>
+                </Grid>
+              </Grid>
+          )
+        }
+      case "dropdown":
+        if(viewHints.values) {
+          return (
+              <FormControl classes={{root: classes.inputField}}>
+                <InputLabel id={controlId}>{viewHints.name}</InputLabel>
+                <Select labelId={controlId}
+                        readOnly={viewHints.readonly || props.readonly}
+                        value={maybeValues ? maybeValues.length > 0 ? maybeValues[0] : "" : ""}
+                        onChange={(event)=>props.valueDidChange(fieldname, [event.target.value as string])}
+                >
+                  <MenuItem value="">(not set)</MenuItem>
+                  {
+                    viewHints.values.map( (dataPair, index)=>
+                      <MenuItem value={dataPair.key}>{dataPair.value}</MenuItem>
+                    )
+                  }
+                </Select>
+              </FormControl>
+          )
+        } else {
+          return (
+              <Grid
+                  container
+                  direction="row"
+                  justify="flex-start"
+                  alignItems="flex-start"
+                  spacing={1}
+              >
+                <Grid item sm={6}>
+                  <Typography>{viewHints.name}</Typography>
+                </Grid>
+
+                <Grid item sm={6}>
+                  <Typography
+                      variant="caption"
+                      id={controlId}
+                      classes={{
+                        root: classes.root,
+                      }}
+                  >
+                    Warning: field is a dropdown but has no values configured
+                  </Typography>
+                </Grid>
+              </Grid>
+          )
+        }
       case "textarea":
         return (
           <FormControl classes={{ root: classes.inputField }}>
