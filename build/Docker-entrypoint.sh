d2a9c9255d39438554f33f9aad03de777886acaf
Andy Gallagher <andy.gallagher@theguardian.com>
2020-08-19 13:30:01 +0100

    bugfix for runlocal.sh with new docker image configuration

--- build/Docker-entrypoint.sh
+++ build/Docker-entrypoint.sh
@@ -12,7 +12,7 @@ fi
 
 mkdir -p usr/share/nginx/html/config
 cp -avL /etc/config/* /usr/share/nginx/html/config
-cat /usr/share/nginx/html/index.template.html | sed 's/{{ DEPLOYMENTPATH }}/'$DEPLOYMENTPATH/ > /usr/share/nginx/html/index.html
+cat /usr/share/nginx/html/index.template.html | sed 's.{{ DEPLOYMENTPATH }}.'$DEPLOYMENTPATH. > /usr/share/nginx/html/index.html
 rm -f /usr/share/nginx/html/index.template.html
 
 echo Running server....
--- build/runlocal.sh
+++ build/runlocal.sh
@@ -4,4 +4,4 @@ docker build . -t guardianmultimedia/pluto-mediabrowser:DEV
 echo -----------------------
 echo Starting up server. Try accessing at http://localhost:8000........
 
-docker run --rm -p 8000:80 -v $PWD/bundle.js:/usr/share/nginx/html/bundle.js guardianmultimedia/pluto-mediabrowser:DEV
+docker run --rm -it -p 8000:80 -e DEPLOYMENTPATH=http:\/\/localhost:8000\/ -v $PWD/bundle.js:/usr/share/nginx/html/bundle.js guardianmultimedia/pluto-mediabrowser:DEV
